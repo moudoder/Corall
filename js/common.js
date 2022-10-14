d8a22c5a7ac1d04a0011827ac61508408c4937bf
moudoder <39887984+moudoder@users.noreply.github.com>
2022-10-14 18:39:18 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('.works-slider-big').slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    swipe: false,
+  });
+
+
+  $(".scroll-link").on("click", function (event) {
+    event.preventDefault();
+    let id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+})
+//;
