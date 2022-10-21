cb732c91a34395e4d3de371936434da0d465e73c
moudoder <39887984+moudoder@users.noreply.github.com>
2022-10-21 15:57:44 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  $('.works-slider-big').on('init', function(event, slick){
+    AOS.init();
+  });
+  
   $('.works-slider-big').slick({
     infinite: false,
     slidesToShow: 1,
@@ -8,13 +12,16 @@ $(document).ready(function () {
     cssEase: 'linear',
     swipe: false,
   });
-
-
   $(".scroll-link").on("click", function (event) {
     event.preventDefault();
     let id  = $(this).attr('href'),
     top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 1500);
   });
+  
+  $('.politics-item').on('click', function() {
+    $('.politics-item').removeClass('active');
+    $(this).addClass('active');
+    return false;
+  })
 })
-//;
